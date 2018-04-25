9f312ecc578efc980c6785d051e376a42d7c0b5f
Daniel <32126961+GaloisField94@users.noreply.github.com>
2018-04-25 12:53:22 +0200

    Add files via upload

---
+++ CertA.cpp
@@ -0,0 +1,182 @@
+#include <ElCurve.h>
+#include <gcrypt.h>
+#include <fstream>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define chosenHashFunction GCRY_MD_SHA512
+
+uint8_t MAX_ZZ_BYTES, it;
+uint16_t p_bits;
+ZZ p, n, a, b, Gx, Gy, tmpX, tmpY, dCA, k, e, r;
+ECpoint *theGenerator, *QCA, *RU, *kG, *PU, *condition;
+FILE *fp;
+std::string tmp_str, fifoFileName;
+std::ofstream writeFile;
+std::ifstream readFile;
+char *buffer1, *buffer2, tmp_c;
+
+int main(int argc, char const *argv[]) {
+  if(!gcry_check_version(GCRYPT_VERSION)) {
+    fputs ("libgcrypt version mismatch\n", stderr);
+    exit(2);
+  }
+
+  SetSeed(RandomBnd(conv<ZZ>("9409250011798120900227")));
+
+  readFile.open("CApublicFile.txt");
+  if(!readFile.is_open()) {
+    std::cout << "Couldn't open desired file!" << "\n";
+    return 0;
+  }
+
+  getline(readFile, tmp_str);
+  p_bits = atoi(tmp_str.c_str());
+
+  switch(p_bits) {
+    case 192:
+      p = conv<ZZ>("6277101735386680763835789423207666416083908700390324961279");
+      n = conv<ZZ>("6277101735386680763835789423176059013767194773182842284081");
+      a = p-3;
+      b = conv<ZZ>("2455155546008943817740293915197451784769108058161191238065");
+      Gx = conv<ZZ>("602046282375688656758213480587526111916698976636884684818");
+      Gy = conv<ZZ>("174050332293622031404857552280219410364023488927386650641");
+      MAX_ZZ_BYTES = 24;
+      break;
+    case 224:
+      p = conv<ZZ>("26959946667150639794667015087019630673557916260026308143510066298881");
+      n = conv<ZZ>("26959946667150639794667015087019625940457807714424391721682722368061");
+      a = p-3;
+      b = conv<ZZ>("18958286285566608000408668544493926415504680968679321075787234672564");
+      Gx = conv<ZZ>("19277929113566293071110308034699488026831934219452440156649784352033");
+      Gy = conv<ZZ>("19926808758034470970197974370888749184205991990603949537637343198772");
+      MAX_ZZ_BYTES = 28;
+      break;
+    case 256:
+      p = conv<ZZ>("115792089210356248762697446949407573530086143415290314195533631308867097853951");
+      n = conv<ZZ>("115792089210356248762697446949407573529996955224135760342422259061068512044369");
+      a = p-3;
+      b = conv<ZZ>("41058363725152142129326129780047268409114441015993725554835256314039467401291");
+      Gx = conv<ZZ>("48439561293906451759052585252797914202762949526041747995844080717082404635286");
+      Gy = conv<ZZ>("36134250956749795798585127919587881956611106672985015071877198253568414405109");
+      MAX_ZZ_BYTES = 32;
+      break;
+    case 384:
+      p = conv<ZZ>("39402006196394479212279040100143613805079739270465446667948293404245721771496870329047266088258938001861606973112319");
+      n = conv<ZZ>("39402006196394479212279040100143613805079739270465446667946905279627659399113263569398956308152294913554433653942643");
+      a = p-3;
+      b = conv<ZZ>("27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575");
+      Gx = conv<ZZ>("26247035095799689268623156744566981891852923491109213387815615900925518854738050089022388053975719786650872476732087");
+      Gy = conv<ZZ>("8325710961489029985546751289520108179287853048861315594709205902480503199884419224438643760392947333078086511627871");
+      MAX_ZZ_BYTES = 48;
+      break;
+    case 521:
+      p = conv<ZZ>("6864797660130609714981900799081393217269435300143305409394463459185543183397656052122559640661454554977296311391480858037121987999716643812574028291115057151");
+      n = conv<ZZ>("6864797660130609714981900799081393217269435300143305409394463459185543183397655394245057746333217197532963996371363321113864768612440380340372808892707005449");
+      a = p-3;
+      b = conv<ZZ>("1093849038073734274511112390766805569936207598951683748994586394495953116150735016013708737573759623248592132296706313309438452531591012912142327488478985984");
+      Gx = conv<ZZ>("2661740802050217063228768716723360960729859168756973147706671368418802944996427808491545080627771902352094241225065558662157113545570916814161637315895999846");
+      Gy = conv<ZZ>("3757180025770020463545507224491183603594455134769762486694567779615544477440556316691234405012945539562144444537289428522585666729196580810124344277578376784");
+      MAX_ZZ_BYTES = 66;
+      break;
+    default:
+      std::cout << "Choose a proper NIST elliptic curve: 192, 224, 256, 384, or 521" << std::endl;
+      return 0;
+  }
+
+  ElCurve *theCurve = new ElCurve(p, a, b);
+  theGenerator = new ECpoint(Gx, Gy);
+
+  std::getline(readFile, tmp_str);
+  tmpX = conv<ZZ>(tmp_str.c_str());
+  std::getline(readFile, tmp_str);
+  tmpY = conv<ZZ>(tmp_str.c_str());
+  readFile.close();
+  QCA = new ECpoint(tmpX, tmpY);
+  //QCA->printlnPoint();
+
+  readFile.open("CAprivateFile.txt");
+  if(!readFile.is_open()) {
+    std::cout << "Couldn't open desired file!" << "\n";
+    return 0;
+  }
+  std::getline(readFile, tmp_str);
+  dCA = conv<ZZ>(tmp_str.c_str());
+  readFile.close();
+  //ECpoint *QCA2 = theCurve->mulPoint(theGenerator, dCA);
+  //QCA2->printlnPoint(); checked, it's correct
+
+  //receive request
+  umask(0);
+  mknod("mkfifoRUx", S_IFIFO|0666, 0);
+  std::cout << "CA ready to receive a new request" << "\n";
+  fp = fopen("mkfifoRUx", "r");
+  buffer1 = (char *)malloc(sizeof *buffer1 * (MAX_ZZ_BYTES + 1));
+  it = 0;
+  while((tmp_c = getc(fp)) != EOF) buffer1[it++] = tmp_c;
+  //std::cout << "received" << buffer1 << "\n";
+  ZZFromBytes(tmpX, (unsigned char*)buffer1, MAX_ZZ_BYTES);
+  //std::cout << "Received " << tmpX << "\n";
+  fclose(fp);
+  unlink("mkfifoRUx");
+  umask(0);
+  mknod("mkfifoRUy", S_IFIFO|0666, 0);
+  fp = fopen("mkfifoRUy", "r");
+  buffer2 = (char *)malloc(sizeof *buffer1 * (MAX_ZZ_BYTES + 1));
+  it = 0;
+  while((tmp_c = getc(fp)) != EOF) buffer2[it++] = tmp_c;
+  //std::cout << "received" << buffer2 << "\n";
+  ZZFromBytes(tmpY, (unsigned char*)buffer2, MAX_ZZ_BYTES);
+  //std::cout << "Received " << tmpY << "\n";
+  fclose(fp);
+  unlink("mkfifoRUy");
+
+  //after receiving request
+  RU = new ECpoint(tmpX, tmpY);
+  std::cout << "Received request RU = ";
+  RU->printlnPoint();
+  bool kIsBad = false;
+  unsigned char *PUx_str = (unsigned char *)malloc(sizeof *PUx_str * MAX_ZZ_BYTES);
+  unsigned char *PUy_str = (unsigned char *)malloc(sizeof *PUy_str * MAX_ZZ_BYTES);
+  unsigned char *CertU = (unsigned char *)malloc(sizeof *CertU * MAX_ZZ_BYTES * 2);
+  unsigned char *e_str = (unsigned char *)malloc(sizeof *e_str * gcry_md_get_algo_dlen(chosenHashFunction));
+  do {
+    k = RandomBnd(n);
+    kG = theCurve->mulPoint(theGenerator, k);
+    //PU=RU+kG
+    PU = theCurve->addPoints(RU, kG);
+    //CertU=Enc(PU)
+    BytesFromZZ(PUx_str, PU->getX(), MAX_ZZ_BYTES);
+    BytesFromZZ(PUy_str, PU->getY(), MAX_ZZ_BYTES);
+    for(int i = 0; i < MAX_ZZ_BYTES; i++) {
+      CertU[i] = PUx_str[i];
+      CertU[i+MAX_ZZ_BYTES] = PUy_str[i];
+    }
+    //e=Hn(CertU)
+    gcry_md_hash_buffer(chosenHashFunction, e_str, CertU, MAX_ZZ_BYTES * 2);
+    ZZFromBytes(e, e_str, gcry_md_get_algo_dlen(chosenHashFunction));
+    if(MAX_ZZ_BYTES < gcry_md_get_algo_dlen(chosenHashFunction)) trunc(e, e, p_bits);
+    //if ePU+QCA=inf find better k
+    condition = theCurve->addPoints(theCurve->mulPoint(PU, e), QCA);
+    if(condition->isInfinite()) kIsBad = true;
+  } while(kIsBad);
+  AddMod(r, MulMod(e, k, n), dCA, n);
+  //send (r,CertU)
+  unsigned char *r_str = (unsigned char *)malloc(sizeof *r_str * MAX_ZZ_BYTES);
+  BytesFromZZ(r_str, r, MAX_ZZ_BYTES);
+
+  fp = fopen("mkfifor", "w");
+  fputs((char *)r_str, fp);
+  fclose(fp);
+  fp = fopen("mkfifoCertU", "w");
+  fputs((char *)CertU, fp);
+  fclose(fp);
+  std::cout << "Calculated e=Hn(CertU), e = " << e << "\n";
+  std::cout << "Calculated PU and encoded it into CertU, PU = ";
+  PU->printlnPoint();
+  std::cout << "Sent response (r, CertU), where r = " << r << "\n";
+
+  return 0;
+}
